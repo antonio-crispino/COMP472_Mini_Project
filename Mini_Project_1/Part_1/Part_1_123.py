92ae503caddb5edfb2ef39a55eb8e03c39b44ba0
antonio-crispino <77855308+antonio-crispino@users.noreply.github.com>
2022-10-02 16:44:47 -0400

    Do part 1.3

---
+++ Mini_Project_1/Part_1/Part_1_123.py
@@ -0,0 +1,74 @@
+import gzip
+import json
+import matplotlib.pyplot as plt
+
+posts = []
+posts_count = 0
+emotions = {}
+sentiments = {}
+
+with gzip.open('Mini_Project_1\Dataset\goemotions.json.gz', 'rb') as f:
+  # "posts" is an array of posts, where each post is an array 
+  # that contains a post's text, its emotion, and its sentiment.
+  posts = json.load(f)
+
+  for post in posts:
+    posts_count += 1
+    emotion = post[1]
+    sentiment = post[2]
+
+    if emotion not in emotions:
+      emotions[emotion] = 1
+    else:
+      emotions[emotion] += 1
+    
+    if sentiment not in sentiments:
+      sentiments[sentiment] = 1
+    else:
+      sentiments[sentiment] += 1
+
+for emotion, occurance in emotions.items():
+  emotions[emotion] = {"occurance": occurance, "frequency": (occurance/posts_count)*100}
+
+for sentiment, occurance in sentiments.items():
+  sentiments[sentiment] = {"occurance": occurance, "frequency": (occurance/posts_count)*100}
+
+# emotions bar graph (occurances)
+plt.clf()
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.30)
+plt.bar(emotions.keys(), list(pair["occurance"] for pair in emotions.values()))
+plt.title('Occurences of Emotions')
+plt.xlabel('Emotions')
+plt.ylabel('Occurences')
+plt.savefig("Mini_Project_1\Part_1\Figures\Occurences_of_Emotions.pdf")
+
+# emotions bar graph (frequencies)
+plt.clf()
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.30)
+plt.bar(emotions.keys(), list(pair["frequency"] for pair in emotions.values()))
+plt.title('Frequencies of Emotions')
+plt.xlabel('Emotions')
+plt.ylabel('Frequency (%)')
+plt.savefig("Mini_Project_1\Part_1\Figures\Frequency_of_Emotions.pdf")
+
+# sentiments bar graph (occurances)
+plt.clf()
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.30)
+plt.bar(sentiments.keys(), list(pair["occurance"] for pair in sentiments.values()))
+plt.title('Occurences of Sentiments')
+plt.xlabel('Sentiments')
+plt.ylabel('Occurences')
+plt.savefig("Mini_Project_1\Part_1\Figures\Occurences_of_Sentiments.pdf")
+
+# sentiments bar graph (frequencies)
+plt.clf()
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.30)
+plt.bar(sentiments.keys(), list(pair["frequency"] for pair in sentiments.values()))
+plt.title('Frequencies of Sentiments')
+plt.xlabel('Sentiments')
+plt.ylabel('Frequency (%)')
+plt.savefig("Mini_Project_1\Part_1\Figures\Frequency_of_Sentiments.pdf")
