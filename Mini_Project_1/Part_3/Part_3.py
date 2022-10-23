b4621540208904f82ef737f15a6e71c9350eb5d0
antonio-crispino <77855308+antonio-crispino@users.noreply.github.com>
2022-10-22 21:07:25 -0400

    Add part 3.2

--- Mini_Project_1/Part_3/Part_3.py
+++ Mini_Project_1/Part_3/Part_3.py
@@ -27,3 +27,23 @@ sentiment_array = np.array([post[2] for post in posts_array])
 
 pretrained_embedding_model = load('word2vec-google-news-300')
 # print(pretrained_embedding_model) #TESTING
+
+# ----------
+# Part 3.2
+# ----------
+
+# if 'punkt' is not downloaded, download it
+try:
+  nltk.data.find('tokenizers/punkt')
+except LookupError:
+  nltk.download('punkt')
+
+# create 2d-array where sub-arrays are tokens from each post (content)
+tokenized_content_array = [word_tokenize(content) for content in content_array]
+# flatten 2d-array to create an array of every token from every sub-array
+all_word_tokens = np.array([token for tokenized_content in tokenized_content_array for token in tokenized_content])
+# Number of tokens in training set
+print("Number of tokens in training set: ", len(all_word_tokens))
+
+# print(tokenized_content_array) #TESTING
+# print(all_word_tokens) #TESTING
