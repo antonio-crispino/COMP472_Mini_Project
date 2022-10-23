f767216a50fa321986a1dad6030451200e76b19e
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2022-10-23 09:22:32 -0400

    part3 embeddings complete

--- Mini_Project_1/Part_3/Part_3.py
+++ Mini_Project_1/Part_3/Part_3.py
@@ -6,10 +6,12 @@ from gensim.downloader import load
 from gensim.models import Word2Vec
 import nltk
 from nltk.tokenize import word_tokenize
+import pandas as pd
+from IPython.display import display
 
 # declare os paths
 main_directory = os.path.join(os.getcwd(), 'Mini_Project_1')
-dataset_path = os.path.join(main_directory, 'Dataset', 'goemotions.json.gz')
+dataset_path = '/Users/mairamalhi/JupyterNotebook/COMP472_Mini_Project/Mini_Project_1/Dataset/goemotions.json.gz'
 
 # load posts into an array
 posts_array = np.array([])
@@ -47,3 +49,33 @@ print("Number of tokens in training set: ", len(all_word_tokens))
 
 # print(tokenized_content_array) #TESTING
 # print(all_word_tokens) #TESTING
+
+
+# ----------
+# Part 3.3
+# ----------
+
+# Take sampel reddit post from the data set
+redditpostsample = ['That', 'looks', 'amazing']
+
+# Two arrays, one for vectorized
+vecter_list=[] # will return embeddings for each word as an array one after the other
+word_filtered_list=[] # check if words exist in other posts (otherwise skip), if so, returns same value as redditpostsample
+
+vecter_list=[pretrained_embedding_model[word] for word in redditpostsample if word in pretrained_embedding_model.index_to_key]
+word_filtered_list=[word for word in redditpostsample if word in pretrained_embedding_model.index_to_key]
+
+# print(vecter_list) #TESTING
+# print(word_filtered_list) #TESTING
+
+# Create a data frame (puts data in clean table) using pandas
+data_frame = pd.DataFrame.from_dict(dict(zip(word_filtered_list,vecter_list)),orient='index')
+
+# Write the new dataframe file to a .json
+json.dump(data_frame.to_dict(), open("embeddings_of_post.json", 'w'))
+json_embedded_posts = pd.read_json("embeddings_of_post.json")
+dict_tokens = json_embedded_posts.to_dict()
+json_embedded_posts.head(3)
+
+# Display the data in console
+# display(data_frame) #TESTING
